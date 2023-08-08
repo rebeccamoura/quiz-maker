8413c93f81c3545cd1aa2e8621a89798d7f0ceb5
Rebecca Moura <rebeccamouradev@gmail.com>
2023-08-08 19:36:40 -0300

    formulário de criação de quiz pronto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -20,7 +21,8 @@ import { CreateQuizComponent } from './create-quiz/create-quiz.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/create-quiz/create-quiz.component.ts
+++ src/app/create-quiz/create-quiz.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormArray, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-quiz',
@@ -6,5 +7,75 @@ import { Component } from '@angular/core';
   styleUrls: ['./create-quiz.component.scss']
 })
 export class CreateQuizComponent {
+
+  quizForm = this.fb.group({
+    name: ['', [Validators.required]],
+    description: ['', [Validators.required]],
+    questions: this.fb.array([
+      this.fb.group({
+        question: ['', [Validators.required]],
+        answers: this.fb.array([
+          this.fb.group({
+            answer: ['', [Validators.required]],
+            isCorrect: true
+          }),
+          this.fb.group({
+            answer: ['', [Validators.required]],
+            isCorrect: false
+          }),
+          this.fb.group({
+            answer: ['', [Validators.required]],
+            isCorrect: false
+          }),
+          this.fb.group({
+            answer: ['', [Validators.required]],
+            isCorrect: false
+          })
+        ])
+      })
+    ])
+  });
+
+  constructor( private fb: FormBuilder) {}
+
+  onSubmit() {
+    console.log('enviado!: ', this.quizForm.value)
+  }
+
+  get questions() {
+    return this.quizForm.get('questions') as FormArray;
+  }
+
+  getAnswers(index: number) {
+    return this.questions.controls[index].get('answers') as FormArray;
+  }
+
+  addQuestion() {
+    this.questions.push(this.fb.group({
+      question: ['', [Validators.required]],
+      answers: this.fb.array([
+        this.fb.group({
+          answer: ['', [Validators.required]],
+          isCorrect: true
+        }),
+        this.fb.group({
+          answer: ['', [Validators.required]],
+          isCorrect: false
+        }),
+        this.fb.group({
+          answer: ['', [Validators.required]],
+          isCorrect: false
+        }),
+        this.fb.group({
+          answer: ['', [Validators.required]],
+          isCorrect: false
+        })
+      ])
+    }))
+  }
+
+  removeQuestion(index: number) {
+    this.questions.removeAt(index)
+  }
 
 }
