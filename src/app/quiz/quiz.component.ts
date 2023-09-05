fb0aef801648f8fd97f54e38abe2bca301e17973
Rebecca Moura <rebeccamouradev@gmail.com>
2023-09-05 19:01:22 -0300

    respostas são embaralhadas antes de serem exibidas e bug de css de card de quiz arrumado

--- src/app/quiz/quiz.component.ts
+++ src/app/quiz/quiz.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { QuizService } from '../services/quiz.service';
 import { Router } from '@angular/router';
-import { Question, Quiz } from '../interfaces/quiz';
+import { Answer, Question, Quiz } from '../interfaces/quiz';
 
 @Component({
   selector: 'app-quiz',
@@ -15,9 +15,21 @@ export class QuizComponent {
   currentQuestion: number = 0;
   correctQuestions: number = 0;
 
+  myArray = [1, 2, 3, 4, 5];
+
   constructor( private quizService: QuizService, private router: Router ) {
     this.quiz = this.quizService.getQuiz();
     this.questions = this.quiz.questions
+  }
+
+  shuffleAnswers(answers: Answer[]) {
+    const shuffledAnswers = [...answers];
+    for (let i = shuffledAnswers.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [shuffledAnswers[i], shuffledAnswers[j]] = [shuffledAnswers[j], shuffledAnswers[i]];
+    }
+
+    return shuffledAnswers;
   }
 
   validateAnswer(isCorrect: any) {
